1f92607fb734f84432903cc5dd6e50cb74fff8fc
chrisb <chris.bacon@themarketinglab.co.uk>
2017-02-06 01:04:49 +0000

    Changed colour to bullet_colour to reflect more accurately what it does

--- classes/Bullet.py
+++ classes/Bullet.py
@@ -14,7 +14,7 @@ class Bullet(Entity):
         super().__init__(display, self.bX, self.bY, "Bullet")
         self.y_change = 0
         self.success_hit = False
-        self.colour = {"Player": (0, 255, 0), "Enemy": (255, 0, 0)}
+        self.bullet_colour = {"Player": (0, 255, 0), "Enemy": (255, 0, 0)}
         self.bullet_speed = {"Player": 10, "Enemy": 7}
 
     def draw(self):
@@ -23,7 +23,7 @@ class Bullet(Entity):
             self.y = self.y - self.bullet_speed["Player"]
         elif self.shooter_type == "Enemy":
             self.y = self.y + self.bullet_speed["Enemy"]
-        pygame.draw.rect(self.display, self.colour[self.shooter_type], (self.x, self.y, 10, 40))
+        pygame.draw.rect(self.display, self.bullet_colour[self.shooter_type], (self.x, self.y, 10, 40))
 
     def destroy(self):
         pygame.draw.rect(self.display, (0, 0, 0), (self.x, self.y, 10, 40))
