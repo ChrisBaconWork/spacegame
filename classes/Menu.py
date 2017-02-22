fd7a4b37754a3e79e45a5fc698a0e96dfc1cff83
chrisb <chris.bacon@themarketinglab.co.uk>
2017-02-22 20:15:51 +0000

    Added music toggle

--- classes/Menu.py
+++ classes/Menu.py
@@ -14,21 +14,34 @@ class Menu(object):
         self.display.blit(start, start_rect)
 
     def start_button(self, clicked="unclicked"):
-        self.display.fill(self.colour[clicked], (200, 200, 200, 200))
+        X = 200
+        Y = 200
+        self.display.fill(self.colour[clicked], (X, Y, 200, 200))
         start = self.fontObj.render('START', True, (255, 255, 255), self.colour[clicked])
         start_rect = start.get_rect(center = (300, 300))
         self.display.blit(start, start_rect)
 
     def quit_button(self, clicked="unclicked"):
-        self.display.fill(self.colour[clicked], (600, 200, 200, 200))
+        X = 600
+        Y = 200
+        self.display.fill(self.colour[clicked], (X, Y, 200, 200))
         quit = self.fontObj.render('Quit', True, (255, 255, 255), self.colour[clicked])
         quit_rect = quit.get_rect(center = (700, 300))
         self.display.blit(quit, quit_rect)
 
-    def draw(self, start="unclicked", quit="unclicked"):
+    def toggle_music_button(self, clicked="unclicked"):
+        X = 400
+        Y = 600
+        self.display.fill(self.colour[clicked], (X, Y, 200, 200))
+        toggle_music = self.fontObj.render('Music', True, (255, 255, 255), self.colour[clicked])
+        toggle_music_rect = toggle_music.get_rect(center = (500, 700))
+        self.display.blit(toggle_music, toggle_music_rect)
+
+    def draw(self, start="unclicked", quit="unclicked", toggle_music="unclicked"):
         self.title()
         self.start_button(start)
         self.quit_button(quit)
+        self.toggle_music_button(toggle_music)
 
     def destroy(self, display):
         display.fill((0, 0, 0), (200, 200, 200, 200))
--- classes/Settings.py
+++ classes/Settings.py
@@ -4,4 +4,3 @@ class Settings(object):
     """This class represents the settings"""
     def __init__(self):
         self.settings = json.load(open('settings.json'))
-        
